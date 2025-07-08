18a6084e6c78ea6b00ba31c5a05c476fc3118b5c
Alessandra <alessandrag408@gmail.com>
2025-07-08 18:04:48 +0200

    add if, else-if and else conditions

--- CSS/dashboard.js
+++ CSS/dashboard.js
@@ -51,3 +51,14 @@ if (numero > 5) {
 } else {
     console.log("La variabile è minore di 5");
 }
+
+let score = parseInt(prompt("Inserisci un numero da 0 a 100"));
+if (score >= 90 && score <= 100) {
+  console.log("voto ottimo");
+} else if (score >= 70 && score <= 89) {
+  console.log("voto buono");
+} else if (score >= 60 && score <= 69) {
+  console.log("voto sufficiente");
+} else if (score <= 59) {
+  console.log("voto insufficiente");
+}
