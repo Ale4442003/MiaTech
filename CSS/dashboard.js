3aeccfcb5ccc799981be34a04af0b3315e73e25e
Alessandra <alessandrag408@gmail.com>
2025-07-09 23:41:22 +0200

    forEach & map

--- CSS/dashboard.js
+++ CSS/dashboard.js
@@ -186,3 +186,15 @@ console.log(Object.entries(student));
 Object.entries(student).forEach(([chiave, valore])=>{
   console.log(`${chiave}: ${valore}`);
 });
+
+let numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach(function(number) {
+  console.log(number * 2);
+});
+
+let squaredNumbers = numbers.map(function(number) {
+  return number * number;
+})
+
+console.log(squaredNumbers);
