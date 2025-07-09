16740423d79185c126ec21520572bd58b57e4d58
Alessandra <alessandrag408@gmail.com>
2025-07-09 22:21:19 +0200

    Counter

--- CSS/dashboard.js
+++ CSS/dashboard.js
@@ -150,3 +150,24 @@ console.log(sum(2));
 console.log(sum(2));
 
 
+function createCounter() {
+  let count = 0;
+
+  return {
+    increment () {
+      count++;
+      return count;
+    },
+    decrement () {
+      count--;
+      return count;
+    }
+  };
+}
+
+
+let counter = createCounter();
+console.log(counter.increment()); 
+console.log(counter.increment()); 
+console.log(counter.decrement()); 
+
