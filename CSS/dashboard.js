6ad50a4ec21ee3550ac80696acd19ee6a7ef67a6
Alessandra <alessandrag408@gmail.com>
2025-07-18 00:02:55 +0200

    metodo privato, protetto, statico

--- CSS/dashboard.js
+++ CSS/dashboard.js
@@ -11,29 +11,29 @@ console.log(number);
 
 let number2 = 20;
 {
-    let number2 = 25;
-    console.log(number2);
+  let number2 = 25;
+  console.log(number2);
 }
 console.log(number2);
 
 let outside = 21;
 {
-    let inside = 9;
-    console.log(inside);
-    console.log(outside);
+  let inside = 9;
+  console.log(inside);
+  console.log(outside);
 }
 //console.log(inside);
 console.log(outside);
 
 
 for (let numero = 0; numero <= 20; numero += 2) {
-    console.log(numero);
+  console.log(numero);
 }
 
-let  decrescente = 9;
+let decrescente = 9;
 while (decrescente >= 1) {
-    console.log (decrescente);
-    decrescente -= 2;
+  console.log(decrescente);
+  decrescente -= 2;
 }
 
 let word = prompt("holii");
@@ -41,15 +41,15 @@ console.log(word.length);
 
 let input;
 do {
-    input = prompt ("Inserisci una parola almeno di 5 lettere");
-} while (input.length<5);
+  input = prompt("Inserisci una parola almeno di 5 lettere");
+} while (input.length < 5);
 console.log("Hai inserito", input);
 
 let numero = 10;
 if (numero > 5) {
-    console.log("La variabile è maggiore di 5");
+  console.log("La variabile è maggiore di 5");
 } else {
-    console.log("La variabile è minore di 5");
+  console.log("La variabile è minore di 5");
 }
 
 let score = parseInt(prompt("Inserisci un numero da 0 a 100"));
@@ -87,13 +87,13 @@ switch (true) {
 console.log(voto);
 
 
-let votazioni = ["0", "1", "2", "3","4", "5"];
+let votazioni = ["0", "1", "2", "3", "4", "5"];
 console.log(votazioni);
 
 let somma = 0;
 
 for (let i = 0; i < votazioni.length; i++) {
-  somma += parseInt(votazioni[i]); 
+  somma += parseInt(votazioni[i]);
 }
 
 console.log("La somma è:", somma);
@@ -102,13 +102,13 @@ for (let reverse = votazioni.length - 1; reverse >= 0; reverse--) {
   console.log(votazioni[reverse]);
 }
 
-let parole =[];
+let parole = [];
 for (let i = 0; i < 5; i++) {
   let parola = prompt("Inserisci massimo 5 parole");
   parole.push(parola);
 }
 for (let i = 0; i < parole.length; i++) {
-  if (parole[i].length % 2 !==0) {
+  if (parole[i].length % 2 !== 0) {
     console.log(parole[i]);
   }
 }
@@ -131,21 +131,21 @@ person.job = "secretary";
 console.log(person.job);
 
 for (let chiave in person) {
-  console.log (chiave + ":" + person [chiave]);
-  }
+  console.log(chiave + ":" + person[chiave]);
+}
 
 
 
 function outerFunction(x, initialValue) {
-  let result= initialValue;
+  let result = initialValue;
 
-  function innerFunction(y){
+  function innerFunction(y) {
     result = result + y;
     return result;
   }
   return innerFunction;
 }
-let sum = outerFunction(4, 4); 
+let sum = outerFunction(4, 4);
 console.log(sum(2));
 console.log(sum(2));
 
@@ -154,11 +154,11 @@ function createCounter() {
   let count = 0;
 
   return {
-    increment () {
+    increment() {
       count++;
       return count;
     },
-    decrement () {
+    decrement() {
       count--;
       return count;
     }
@@ -167,9 +167,9 @@ function createCounter() {
 
 
 let counter = createCounter();
-console.log(counter.increment()); 
-console.log(counter.increment()); 
-console.log(counter.decrement()); 
+console.log(counter.increment());
+console.log(counter.increment());
+console.log(counter.decrement());
 
 
 let student = {
@@ -183,23 +183,23 @@ console.log(Object.keys(student));
 console.log(Object.values(student));
 console.log(Object.entries(student));
 
-Object.entries(student).forEach(([chiave, valore])=>{
+Object.entries(student).forEach(([chiave, valore]) => {
   console.log(`${chiave}: ${valore}`);
 });
 
 let numbers = [1, 2, 3, 4, 5];
 
-numbers.forEach(function(number) {
+numbers.forEach(function (number) {
   console.log(number * 2);
 });
 
-let squaredNumbers = numbers.map(function(number) {
+let squaredNumbers = numbers.map(function (number) {
   return number * number;
 })
 
 console.log(squaredNumbers);
 
-let students =  [
+let students = [
   { name: "Clara", grade: 55 },
   { name: "Ale", grade: 65 },
   { name: "Marco", grade: 30 }
@@ -212,20 +212,37 @@ console.log(passedStudents);
 console.log(failedStudent);
 
 
-let expenses =[25, 21, 13, 7, 33];
+let expenses = [25, 21, 13, 7, 33];
 let total = expenses.reduce(function (accumulator, currentValue) {
   return accumulator + currentValue;
 }, 0);
 
-console.log("Totale spese:", total); 
+console.log("Totale spese:", total);
 
 let words = ["banana", "apple", "cherry", "date"];
-words.sort(); 
+words.sort();
 
 console.log(expenses, words);
 
 class Automobile {
-  constructor (marca, modello, anno) {
+  #calcolaEtà() {
+    let annoNow = new Date().getFullYear();
+    return annoNow - this.anno;
+  }
+
+  #contatoreChiamate = 0;
+
+  static confrontaChilometraggio(auto1, auto2) {
+    if (auto1.chilometraggio > auto2.chilometraggio) {
+      return `${auto1.marca} ${auto1.modello} ha più chilometri (${auto1.chilometraggio} km).`;
+    } else if (auto1.chilometraggio < auto2.chilometraggio) {
+      return `${auto2.marca} ${auto2.modello} ha più chilometri (${auto2.chilometraggio} km).`;
+    } else {
+      return `Entrambe le auto hanno lo stesso chilometraggio (${auto1.chilometraggio} km).`;
+    }
+  }
+
+  constructor(marca, modello, anno) {
     this.marca = marca;
     this.modello = modello;
     this.anno = anno;
@@ -233,7 +250,12 @@ class Automobile {
   }
 
   descrizione() {
+    this.#contatoreChiamate++;
     return `Questa macchina è una ${this.marca} ${this.modello} del ${this.anno}.`;
+  }
+
+  numeroChiamateDescrizione() {
+    return this.#contatoreChiamate;
   }
 
   aggiungiChilometri(km) {
@@ -242,6 +264,18 @@ class Automobile {
 
   mostraChilometraggio() {
     return `${this.chilometraggio} km`;
+  }
+
+  mostraEtà() {
+    return `Questa ${this.marca} ${this.modello} ha ${this.#calcolaEtà()} anni.`;
+  }
+
+  _controllaChilometri() {
+    if (this.chilometraggio > 100000) {
+      return "Chilometraggio troppo alto";
+    } else {
+      return "";
+    }
   }
 }
 
@@ -252,7 +286,9 @@ class Elettrica extends Automobile {
   }
 
   descrizione() {
-    return `Questa macchina è una ${this.marca} ${this.modello} del ${this.anno}, con ${this.chilometraggio} km percorsi e un'autonomia di ${this.autonomia} km.`;
+    const base = super.descrizione();
+    const avviso = this._controllaChilometri();
+    return `${base} Ha percorso ${this.chilometraggio} km e ha un'autonomia di ${this.autonomia} km. ${avviso}`;
   }
 
   ricarica(km) {
@@ -261,12 +297,20 @@ class Elettrica extends Automobile {
 }
 
 Automobile.prototype.saluta = function () {
-  return `Sono una ${this.marca} ${this.modello}.`;
+  return `Ho una ${this.marca} ${this.modello}.`;
 }
 
 let Mercedes = new Elettrica("Mercedes", "SUV", 2024, 400);
+let Kia = new Automobile("Kia", "Picanto", 2024);
+let Toyota = new Elettrica("Toyota", "Yaris", 2010, 400);
 
-let Kia = new Automobile("Kia", "Picanto", "2024");
+Mercedes.aggiungiChilometri(50000);
+Toyota.aggiungiChilometri(20000);
+
+console.log(Automobile.confrontaChilometraggio(Mercedes, Toyota));
+
+Toyota.aggiungiChilometri(150000);
+console.log(Toyota.descrizione());
 
 console.log(Kia.descrizione());
 Kia.aggiungiChilometri(40);
@@ -280,3 +324,7 @@ console.log(Mercedes.descrizione());
 
 console.log(Kia.saluta());
 console.log(Mercedes.saluta());
+
+console.log("Mercedes descrizione chiamata:", Mercedes.numeroChiamateDescrizione());
+console.log("Kia descrizione chiamata:", Kia.numeroChiamateDescrizione());
+console.log("Toyota descrizione chiamata:", Toyota.numeroChiamateDescrizione());
