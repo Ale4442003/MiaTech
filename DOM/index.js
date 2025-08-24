d27d07a0dc7ddbc4eafcd68870ccab893edd44e6
Alessandra <alessandrag408@gmail.com>
2025-08-24 14:41:10 +0200

    add a list and using createElement()

--- DOM/index.js
+++ DOM/index.js
@@ -1,8 +1,19 @@
 const paragraph = document.getElementById("text");
-console.log(paragraph);
 const paragraph2 = document.querySelector(".text2");
-console.log(paragraph2);
 const myText = document.getElementById("description");
+const list = document.createElement("ul");
+const user = document.createElement("li");
+user.innerText = "First user";
+const user2 = document.createElement("li");
+user2.innerText = "Second user";
+const user3 = document.createElement("li");
+user3.innerText = "Third user";
+
+list.append(user, user2, user3);
+document.body.append(list);
+
+console.log(paragraph);
+console.log(paragraph2);
 console.log(myText);
 
 myText.innerHTML = "This is a new paragraph using innerHTML with a botton <button>Click</button>";
