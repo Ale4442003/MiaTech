ab04623aa0ccc9dccf33c1a5f661c30158d20016
Alessandra <alessandrag408@gmail.com>
2025-08-24 13:56:50 +0200

    Using inner.Text

--- DOM/index.js
+++ DOM/index.js
@@ -2,3 +2,7 @@ const paragraph = document.getElementById("text");
 console.log(paragraph);
 const paragraph2 = document.querySelector(".text2");
 console.log(paragraph2);
+const myText = document.getElementById("description");
+console.log(myText);
+
+myText.innerText = "This is a new paragraph using innerText";
