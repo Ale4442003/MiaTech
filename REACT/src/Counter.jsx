6d575f10addc9dfbd2694a6f646fd3efa0da1727
Alessandra <alessandrag408@gmail.com>
2025-09-12 23:44:58 +0200

    counter decrement

--- REACT/src/Counter.jsx
+++ REACT/src/Counter.jsx
@@ -4,9 +4,12 @@ export default function Counter() {
     const [count, setCount] = useState(0);
 
     const increment = () => setCount(count + 1);
+    const decrement = () => setCount(count - 1);
+
 
     return <div>
         <div>Count: {count}</div>
         <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
         </div>
 }
