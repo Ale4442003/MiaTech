08e8466fffd90032759b39ec5acceb10b6d147ad
Alessandra <alessandrag408@gmail.com>
2025-09-12 22:35:33 +0200

    add counter

--- REACT/src/App.jsx
+++ REACT/src/App.jsx
@@ -1,14 +1,22 @@
-import { Component } from "react";     
-import HelloWorld from "./Helloworld.jsx";
+// import { Component } from "react";     
+// import HelloWorld from "./Helloworld.jsx";
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <HelloWorld />
-      </>
-    );
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//       <>
+//         <HelloWorld />
+//       </>
+//     );
+//   }
+// }
+
+// export default App;
+
+import Counter from "./Counter";
+
+function App() {
+  return <Counter />;
 }
 
 export default App;
