9908edfd3fc5d1341a150c11270c05e21aa62a51
Alessandra <alessandrag408@gmail.com>
2025-09-12 23:56:13 +0200

    add button reset counter

--- REACT/src/Counter.jsx
+++ REACT/src/Counter.jsx
@@ -5,11 +5,12 @@ export default function Counter() {
 
     const increment = () => setCount(count + 1);
     const decrement = () => setCount(count - 1);
-
+    const reset = () => setCount(0);
 
     return <div>
         <div>Count: {count}</div>
         <button onClick={increment}>Increment</button>
         <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>reset</button>
         </div>
 }
