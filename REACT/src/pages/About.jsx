f2e2713dbdaa391c1e69ffa9a9c59a0f829df97c
Alessandra <alessandrag408@gmail.com>
2025-09-29 22:26:27 +0200

    add button go Home with useNavigate hook

--- REACT/src/pages/About.jsx
+++ REACT/src/pages/About.jsx
@@ -1,10 +1,17 @@
+import {  useNavigate } from "react-router-dom";
+
 const About = () => {
+  const navigate = useNavigate();
+
+  const goHome = () => navigate("/");
+  
   return (
     <>
     <h1>About</h1>
     <p>This application contains a Todo list</p>
+    <button onClick={goHome}>Go Home</button>
     </>
-  )
-}
+  );
+};
 
 export default About;
