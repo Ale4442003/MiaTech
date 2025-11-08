f394207bc0363929275c4bc4e0d6a15542346f10
Alessandra <alessandrag408@gmail.com>
2025-11-08 12:09:05 +0100

    Move Todo object to index.ts

--- todo-list/src/index.ts
+++ todo-list/src/index.ts
@@ -1,2 +1,9 @@
-const myName: string = 'Ale'; 
-console.log(myName);
+// const myName: string = 'Ale'; 
+// console.log(myName);
+import { Todo } from "./types";
+
+const myTodo: Todo = {
+    id: 1,
+    title: "Study Typescript",
+    completed: false,
+};
--- todo-list/src/types.ts
+++ todo-list/src/types.ts
@@ -1,14 +1,8 @@
 //un'interfaccia definisce la struttura che un oggetto deve avere
 //dice quali proprietà esistono e di che tipo sono
 //non crea un oggetto, è solo uno "schema" da seguire (lo controlla)
-interface Todo {
+export interface Todo {
     id: number,
     title: string,
     completed: boolean,
 }
-
-const myTodo: Todo = {
-    id: 1,
-    title: "Study Typescript",
-    completed: false,
-};
