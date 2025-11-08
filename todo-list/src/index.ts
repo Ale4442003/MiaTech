dc1020110fb7a20b4be7503e7ae796b204648c91
Alessandra <alessandrag408@gmail.com>
2025-11-08 13:17:06 +0100

    add addTodo function and initialize todos array

--- todo-list/src/index.ts
+++ todo-list/src/index.ts
@@ -7,3 +7,18 @@ const myTodo: Todo = {
     title: "Study Typescript",
     completed: false,
 };
+
+const todos: Todo[] = [];
+
+//const nomeFunzione = (parametro :Tipo (cosa entra)): cosa esce => { funzioni const}
+//:Todo → deve restituire un oggetto che rispetta la forma dell'interface Todo
+const addTodo = (title:string): Todo => {
+    //newTodo deve rispettare l'interfaccia Todo
+    const newTodo: Todo = {
+        id:todos.length + 1, //id sempre diverso
+        title: title,
+        completed:false,
+    };
+    todos.push(newTodo);
+    return(newTodo);
+}
