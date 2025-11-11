f054e27cf42e8645b32a1fd5f8c3a5e0aaf34e55
Alessandra <alessandrag408@gmail.com>
2025-11-11 23:29:41 +0100

    add User class with todos list andaddTodo method

--- todo-list/src/User.ts
+++ todo-list/src/User.ts
@@ -1,11 +1,18 @@
+import { Todo } from "./types";
+
 class User {
     id: string;
     name: string;
-    email?: string | undefined
+    email?: string | undefined;
+    todos: Todo[] = [];
 
     constructor(id: string, name: string, email?: string) {
         this.id = id;
         this.name = name;
         this.email = email;
     }
+    addTodo(todo: Todo) {
+        this.todos.push(todo);
+    }
 }
+export { User }
